733d3ed4f337f878ed0b29dcaf462d1ca5ab4486
Noble Tom Mathew <nobletm@meta.com>
2025-06-19 18:53:32 -0700

    added functionality for light and dark mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from "./components/ThemeProvider";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <ThemeProvider>
         <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
